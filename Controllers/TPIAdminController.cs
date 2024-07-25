a6099c6da4ac5851a026b4bb85fce7221cfa3bd3
Saad-6 <131909921+Saad-6@users.noreply.github.com>
2024-07-25 19:57:45 +0500

    Fetching data from a public API

--- Controllers/TPIAdminController.cs
+++ Controllers/TPIAdminController.cs
@@ -1,8 +1,14 @@
-﻿using CommerceBuilder.Web.Mvc;
+﻿using AbleCommerce.Areas.Admin.Models;
+using CommerceBuilder.Catalog;
+using CommerceBuilder.Common;
+using CommerceBuilder.Products;
+using CommerceBuilder.Web.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using TPIPlugin;
@@ -10,11 +16,13 @@ using TPIPlugin;
 public class TPIAdminController : AbleAdminController
     {
         HttpClient _client;
-
-        string _url = "http://api.tvmaze.com/search/shows?q=";
-        public TPIAdminController()
+        IProductRepository _productRepository;
+        ICategoryRepository _categoryRepository;
+        public TPIAdminController(IProductRepository Repository,ICategoryRepository categoryRepository)
         {
             _client = new HttpClient();
+            _productRepository = Repository;
+            _categoryRepository = categoryRepository;
         }
         public ActionResult Index()
         {
@@ -22,22 +30,44 @@ public class TPIAdminController : AbleAdminController
            return View("~/Plugins/TPIPlugin/Views/Index.cshtml");
 
         }
-        public async Task<ActionResult> GetShow(string query)
+        public async Task<ActionResult> GetShow(string query="")
         {
             HttpResponseMessage response = await _client.GetAsync($"http://api.tvmaze.com/search/shows?q={query}");
             response.EnsureSuccessStatusCode();
             string jsonResponse = await response.Content.ReadAsStringAsync();
             List<TvMaze> tvMazeList = JsonConvert.DeserializeObject<List<TvMaze>>(jsonResponse);
-            foreach (var show in tvMazeList) 
-            {
-            if (show.show.image == null)
-            {
-                tvMazeList.Remove(show);
-            }
-            }
-
             return PartialView("~/Plugins/TPIPlugin/Views/_ShowsList.cshtml",tvMazeList);
         }
+        public ActionResult AddNewProduct(BasicProductModel model) 
+        {
+        if (model == null)
+        {
+            return Json(new { success = false });
+        }
+        var category = _categoryRepository.FindByName(model.Category).FirstOrDefault();
+        if (category == null)
+        {
+            category = new Category();
+            category.Name = model.Category;
+            category.Store = AbleContext.Current.Store;
+            _categoryRepository.Save(category);
+        }
+        Product product = new Product();
+        product.Name = model.Name;
+        product.Sku = model.Sku;
+        product.ImageUrl = model.ImageUrl;
+        product.ThumbnailUrl = model.ImageUrl;
+        product.Categories.Add(category.Id);
+        product.Description = model.Description;
+        product.Price = model.Price;
+        product.CostOfGoods = model.Cost;
+        product.Unit = model.Unit;
+        product.Store = AbleContext.Current.Store;
+        _productRepository.Save(product);
+
+        return Json(new { success = true });
+    }
+
 
     }
 
---
+++ Models/BasicProductModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Build.Framework;
+
+
+public class BasicProductModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Sku { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+        public string Unit { get; set; }
+        public int CatrgoryId  { get; set; }
+
+        
+
+
+    }
